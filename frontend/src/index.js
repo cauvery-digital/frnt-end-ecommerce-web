36399f1313d85a9a14d5d7e268dd853edeeb0081
Nilesh <geetaelectronicsandinfosys@gmail.com>
2025-05-14 15:58:08 +0530

    first commit

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,7 +8,7 @@ import { positions, transitions, Provider as AlertProvider } from "react-alert";
 import AlertTemplate from "react-alert-template-basic";
 
 const options = {
-  timeout: 100000,
+  timeout: 5000,
   position: positions.BOTTOM_CENTER,
   transition: transitions.SCALE,
 };
